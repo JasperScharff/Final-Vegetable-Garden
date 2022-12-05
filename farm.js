8f87215b1c949fce7099293074034a1ab2a32bd1
Jasper Scharff <96122513+JasperScharff@users.noreply.github.com>
2022-12-05 16:10:08 +0100

    Update farm.js

--- farm.js
+++ farm.js
@@ -1,115 +1,111 @@
+const getCostsForCrop = (inputYield, resultWind, resultSun) => {
+  return inputYield * (resultWind / 100 + 1) * (resultSun / 100 + 1);
+};
+
+const calculateYield = (wind, sun, inputyield) => {
+  // De volledige herhalende if statements in deze functie zetten
+  if (environmentFactors.wind === "little") {
+    const wind = input.factor.wind.little;
+    const sun = input.factor.sun.low;
+
+    totaleOpbrengstLittleWind =
+      input.yield * (wind / 100 + 1) * (sun / 100 + 1);
+    console.log(totaleOpbrengstLittleWind);
+  } else if (environmentFactors.wind === "medium") {
+    const wind = input.factor.wind.medium;
+    const sun = input.factor.sun.low;
+
+    totaleOpbrengstMediumWind = getCostsForCrop(inputYield, wind, sun);
+    console.log(totaleOpbrengstMediumWind);
+  } else if (environmentFactors.wind === "much") {
+    const wind = input.factor.wind.much;
+    const sun = input.factor.sun.low;
+
+    totaleOpbrengstMuchWind = input.yield * (wind / 100 + 1) * (sun / 100 + 1);
+    console.log(totaleOpbrengstMuchWind);
+  }
+};
+
 const getYieldForPlant = (input, environmentFactors) => {
   console.log("Get yield from crop value: ", input.yield); //   plantOutput = 30;
 
-  if (environmentFactors !== undefined) {
-    // als er wel environmentFactors zijn
-    console.log("The enviromental factors: ", environmentFactors);
-    if (environmentFactors.sun === "low") {
-      if (environmentFactors.wind === "little") {
-        //   console.log(
-        //     "Low sun x aantal opbrengst",
-        //     input.factor.sun.low * input.yield
-        //   );
-        //   console.log(
-        //     "Little wind x aantal opbrengst",
-        //     input.factor.wind.little * input.yield
-        //   );
-        // //   console.log("Wind is low percentage", input.factor.wind.little);
-        //   console.log("Sun is low percentage", input.factor.sun.low);
-        const resultLittleWind = input.factor.wind.little;
-        const resultLowSun = input.factor.sun.low;
+  if (environmentFactors == undefined) {
+    return input.yield;
+  }
 
-        totaleOpbrengstLittleWind =
-          input.yield * (resultLittleWind / 100 + 1) * (resultLowSun / 100 + 1);
-        console.log(totaleOpbrengstLittleWind);
-      } else if (environmentFactors.wind === "medium") {
-        const resultMediumWind = input.factor.wind.medium;
-        const resultLowSun = input.factor.sun.low;
+  // als er wel environmentFactors zijn
+  console.log("The enviromental factors: ", environmentFactors);
+  if (environmentFactors.sun === "low") {
+    calculateYield(wind, sun, inputyield);
+  } else if (environmentFactors.sun === "medium") {
+    if (environmentFactors.wind === "little") {
+      const resultLittleWind = input.factor.wind.little;
+      const resultMediumSun = input.factor.sun.medium;
 
-        totaleOpbrengstMediumWind =
-          input.yield * (resultMediumWind / 100 + 1) * (resultLowSun / 100 + 1);
-        console.log(totaleOpbrengstMediumWind);
-      } else if (environmentFactors.wind === "much") {
-        const resultMuchWind = input.factor.wind.much;
-        const resultLowSun = input.factor.sun.low;
+      totaleOpbrengstMediumWind = getCostsForCrop(
+        inputYield,
+        resultLittleWind,
+        resultMediumSun
+      );
+    } else if (environmentFactors.wind === "medium") {
+      const resultMediumWind = input.factor.wind.medium;
+      const resultMediumSun = input.factor.sun.medium;
 
-        totaleOpbrengstMuchWind =
-          input.yield * (resultMuchWind / 100 + 1) * (resultLowSun / 100 + 1);
-        console.log(totaleOpbrengstMuchWind);
-      }
-    } else if (environmentFactors.sun === "medium") {
-        if (environmentFactors.wind === "little") {
-            //   console.log(
-            //     "Low sun x aantal opbrengst",
-            //     input.factor.sun.low * input.yield
-            //   );
-            //   console.log(
-            //     "Little wind x aantal opbrengst",
-            //     input.factor.wind.little * input.yield
-            //   );
-            // //   console.log("Wind is low percentage", input.factor.wind.little);
-            //   console.log("Sun is low percentage", input.factor.sun.low);
-            const resultLittleWind = input.factor.wind.little;
-            const resultMediumSun = input.factor.sun.medium;
-    
-            totaleOpbrengstLittleWind =
-              input.yield * (resultLittleWind / 100 + 1) * (resultMediumSun / 100 + 1);
-            console.log(totaleOpbrengstLittleWind);
-          } else if (environmentFactors.wind === "medium") {
-            const resultMediumWind = input.factor.wind.medium;
-            const resultMediumSun = input.factor.sun.medium;
-    
-            totaleOpbrengstMediumWind =
-              input.yield * (resultMediumWind / 100 + 1) * (resultMediumSun / 100 + 1);
-            console.log(totaleOpbrengstMediumWind);
-          } else if (environmentFactors.wind === "much") {
-            const resultMuchWind = input.factor.wind.much;
-            const resultMediumSun = input.factor.sun.medium;
-    
-            totaleOpbrengstMuchWind =
-              input.yield * (resultMuchWind / 100 + 1) * (resultMediumSun / 100 + 1);
-            console.log(totaleOpbrengstMuchWind);
-          }
-    } else if (environmentFactors.sun === "high") {
-        if (environmentFactors.wind === "little") {
-            //   console.log(
-            //     "Low sun x aantal opbrengst",
-            //     input.factor.sun.low * input.yield
-            //   );
-            //   console.log(
-            //     "Little wind x aantal opbrengst",
-            //     input.factor.wind.little * input.yield
-            //   );
-            // //   console.log("Wind is low percentage", input.factor.wind.little);
-            //   console.log("Sun is low percentage", input.factor.sun.low);
-            const resultLittleWind = input.factor.wind.little;
-            const resultHighSun = input.factor.sun.high;
-    
-            totaleOpbrengstLittleWind =
-              input.yield * (resultLittleWind / 100 + 1) * (resultHighSun / 100 + 1);
-            console.log(totaleOpbrengstLittleWind);
-          } else if (environmentFactors.wind === "medium") {
-            const resultMediumWind = input.factor.wind.medium;
-            const resultHighSun = input.factor.sun.high;
-    
-            totaleOpbrengstMediumWind =
-              input.yield * (resultMediumWind / 100 + 1) * (resultHighSun / 100 + 1);
-            console.log(totaleOpbrengstMediumWind);
-          } else if (environmentFactors.wind === "much") {
-            const resultMuchWind = input.factor.wind.much;
-            const resultHighSun = input.factor.sun.high;
-    
-            totaleOpbrengstMuchWind =
-              input.yield * (resultMuchWind / 100 + 1) * (resultHighSun / 100 + 1);
-            console.log(totaleOpbrengstMuchWind);
-          }
+      totaleOpbrengstMediumWind = getCostsForCrop(
+        inputYield,
+        resultMediumWind,
+        resultMediumSun
+      );
+
+      console.log(totaleOpbrengstMediumWind);
+    } else if (environmentFactors.wind === "much") {
+      const resultMuchWind = input.factor.wind.much;
+      const resultMediumSun = input.factor.sun.medium;
+
+      totaleOpbrengstMediumWind = getCostsForCrop(
+        inputYield,
+        resultMuchWind,
+        resultMediumSun
+      );
+    }
+  } else if (environmentFactors.sun === "high") {
+    if (environmentFactors.wind === "little") {
+      const resultLittleWind = input.factor.wind.little;
+      const resultHighSun = input.factor.sun.high;
+
+      totaleOpbrengstMediumWind = getCostsForCrop(
+        inputYield,
+        resultLittleWind,
+        resultHighSun
+      );
+
+      console.log(totaleOpbrengstLittleWind);
+    } else if (environmentFactors.wind === "medium") {
+      const resultMediumWind = input.factor.wind.medium;
+      const resultHighSun = input.factor.sun.high;
+
+      totaleOpbrengstMediumWind = getCostsForCrop(
+        inputYield,
+        resultMediumWind,
+        resultHighSun
+      );
+
+      console.log(totaleOpbrengstMediumWind);
+    } else if (environmentFactors.wind === "much") {
+      const resultMuchWind = input.factor.wind.much;
+      const resultHighSun = input.factor.sun.high;
+
+      totaleOpbrengstMediumWind = getCostsForCrop(
+        inputYield,
+        resultMuchWind,
+        resultHighSun
+      );
     }
   }
 };
-const getYieldForCrop = (input) => {
-  const outputVariabele = input.crop.yield * input.numCrops;
-  console.log("Get yield for crop output:", outputVariabele);
-  return outputVariabele;
+
+const getYieldForCrop = (input, environmentFactors) => {
+  return getYieldForPlant(input, environmentFactors) * input.numCrops;
 };
 
 const getTotalYield = (input) => {
